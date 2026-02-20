335aec43d345dbf79b74c4e3100f6399fa3f95ec
Gundumogula Dhana Sai <saigundumogula5@gmail.com>
2026-02-20 06:23:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    
     proxy: {
       '/api': {
         target: 'https://unheaved-elina-roughly.ngrok-free.dev',
-        changeOrigin: true
+        secure: false, 
       }
     }
   },
